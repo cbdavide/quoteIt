ff99eaba53d075ade96062782853189393af9c52
cbdavide <david.castelblanco@hotmail.com>
2015-12-14 14:09:57 -0500

    API data implemented in the view.

--- js/app.js
+++ js/app.js
@@ -1,60 +1,20 @@
 (function(){
 
   var $container = $('#container');
+  var quotes = [];
 
-  var ViewQuote = function( quote ) {
+  var ViewQuote = function( text, author ) {
     return `<div class="quote">
     <span class="text">
-      ${quote.text}
+      ${text}
     </span>
     <span class="author">
-      ${quote.author}
+      ${author}
     </span>
   </div>`;
   };
 
-  var Quote = function( text, author ) {
-    this.text = text;
-    this.author = author;
-  }
-
-  var quotes = [
-    new Quote( 'Don\'t wait for the perfect moment, take the moment and make it perfect.', 'David Sayward' ),
-    new Quote( 'Don\'t cry because it\'s over. Smile because it happened.', 'Dr. Seus' ),
-    new Quote( 'He who is brave is free.', 'Lucius Annaeus Seneca' ),
-    new Quote( 'The eye of a human being is a microscope, which makes the world seem bigger than it really is.', 'Khalil Gibran' ),
-    new Quote( 'Be yourself; everyone else is already taken.', 'Oscar Wilde' ),
-    new Quote( 'I\'ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.', 'Maya Angelou' ),
-    new Quote( 'Without music, life would be a mistake.', ' Friedrich Nietzsche' ),
-  ];
-
-  var app = (function() {
-
-    var nQuotes = quotes.length;
-    console.log(nQuotes);
-    var index = 0;
-
-    return {
-      incrementIndex: function(){
-        if( index < (nQuotes - 1) ) {
-          index++;
-        } else {
-          console.log('here');
-          index = 0;
-        }
-      },
-      getIndex: function() {
-        return index;
-      }
-    };
-
-  })();
-
-  var i=0;
   $('body').click(function() {
-    $container.prepend( ViewQuote( quotes[app.getIndex()] ) );
-    app.incrementIndex();
-
     $.ajax({
       url: 'http://api.forismatic.com/api/1.0/',
       jsonp: 'jsonp',
@@ -64,10 +24,16 @@
         lang: 'en',
         format: 'jsonp'
       }
-    }).done(function(data){
-      console.log(JSON.stringify(data));
+    }).done(function(response){
+
+      $container.prepend( ViewQuote( response.quoteText, response.quoteAuthor ) );
+
+      quotes.push( response );
+
+    }).fail(function(err) {
+      console.log('err');
     });
 
   });
-  
+
 })();
