8604ebb9e7cd02995db5a8103b98b930e0403b5d
cbdavide <david.castelblanco@hotmail.com>
2015-12-15 12:21:57 -0500

    Grunt task for minify css

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,12 +10,21 @@ module.exports = function( grunt ) {
           "dist/app.js": "js/app.js"
         }
       }
-  }
+    },
+
+    cssmin: {
+      dist: {
+        files: {
+          'dist/styles.css': ['css/*.css']
+        }
+      }
+    }
 
   });
 
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
   grunt.loadNpmTasks("grunt-babel");
-  grunt.registerTask("default",["babel"]);
+  grunt.registerTask("default",["babel", "cssmin"]);
 
 
 }
